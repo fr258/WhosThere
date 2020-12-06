1efdc2dfae53c47eea911623336f806004615096
fr258 <72672988+fr258@users.noreply.github.com>
2020-12-06 16:40:15 -0500

    Add files via upload

---
+++ Asst3.c
@@ -0,0 +1,299 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+
+#define BACKLOG 5
+#define BUFFSIZE 256
+
+typedef struct listNode
+{
+    char* data;
+    struct listNode *next;
+	
+} Node;
+
+//returns a linked list iterator
+//head must not be null
+typedef struct
+{
+	Node* head;
+	
+} Iterator;
+
+int server(char *port);
+int joke(int sfd);
+int checkValid(int fd, char* input, int key);
+int readIn(int fd, int key);
+char* combine(Node* current, int count);
+void add(Node* head, char* data);
+void deleteList(Node* head);
+int hasNext(Iterator* iter);
+Node* next(Iterator* iter);
+
+
+int main(int argc, char **argv)
+{
+	if (argc != 2) {
+		printf("Usage: %s [port]\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+    (void) server(argv[1]);
+    return EXIT_SUCCESS;
+}
+
+
+int server(char *port)
+{
+    struct addrinfo hint, *address_list, *addr;
+    int error, sfd;
+
+    // initialize hints
+    memset(&hint, 0, sizeof(struct addrinfo)); 
+    hint.ai_family = AF_UNSPEC; //family will be auto selected (?)
+    hint.ai_socktype = SOCK_STREAM; //server is accepting input stream
+    hint.ai_flags = AI_PASSIVE; //server is listening
+	
+    error = getaddrinfo(NULL, port, &hint, &address_list);
+    if (error != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(error));
+        return -1;
+    }
+
+    // attempt to create socket
+    for (addr = address_list; addr != NULL; addr = addr->ai_next) {
+        sfd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+        
+        // if we couldn't create the socket, try the next method
+        if (sfd == -1) {
+            continue;
+        }
+
+        // if we were able to create the socket, try to set it up for
+        // incoming connections;
+        // 
+        // note that this requires two steps:
+        // - bind associates the socket with the specified port on the local host
+        // - listen sets up a queue for incoming connections and allows us to use accept
+        if ((bind(sfd, addr->ai_addr, addr->ai_addrlen) == 0) &&
+            (listen(sfd, BACKLOG) == 0)) {
+            break;
+        }
+
+        // unable to set it up, so try the next method
+        close(sfd);
+    }
+
+    if (addr == NULL) {
+        // we reached the end of result without successfuly binding a socket
+        fprintf(stderr, "Could not bind\n");
+        return -1;
+    }
+
+    freeaddrinfo(address_list);
+
+    // at this point sfd is bound and listening
+    printf("Waiting for connection\n");
+    for (;;) {
+
+        
+        // wait for an incoming connection
+        accept(sfd, NULL, NULL);
+
+        
+        // if we got back -1, it means something went wrong
+        if (sfd == -1) {
+            perror("accept");
+            continue;
+        }		
+		
+		//COMMENCE KNOCK KNOCK
+		
+		//knock knock
+	
+		if(readIn(sfd,1)) //who's there?
+		{
+			close(sfd); //was invalid
+			continue;
+		}
+		
+		//setup
+		
+		if(readIn(sfd,3)) //setup, who?
+		{
+			close(sfd); //was invalid
+			continue;
+		}
+			
+		//punchline
+		
+		if(readIn(sfd,5)) //surprise/disgust
+		{
+			close(sfd); //was invalid
+			continue;
+		}
+			
+		close(sfd);
+		
+    }
+
+    return 0;
+}
+
+
+
+//returns the next value in a linked list
+//iterator passed must not be null
+Node* next(Iterator* iter)
+{
+	if(iter->head != NULL)
+	{
+		Node* temp = iter->head; //save current head
+		iter->head = iter->head->next; //set head in iterator to next node
+		return temp; //return current head
+	}
+	return NULL;
+}
+
+//returns if at lesat one more node with data exists in list
+//iterator must not be null
+int hasNext(Iterator* iter)
+{
+	return (iter->head != NULL);
+}
+
+
+void deleteList(Node* head)
+{
+	Iterator iter = {head}, iter1 = {head};
+	while(hasNext(&iter))
+	{
+		free(next(&iter)->data);
+	}
+	next(&iter1);
+	while(hasNext(&iter1)) //free nodes
+	{
+		free(next(&iter1));
+	}
+}
+
+void add(Node* head, char* data)
+{
+	if(head->data != NULL) 
+	{
+		while(head->next != NULL)
+		{
+			head = head->next;
+		}
+		
+		head->next = malloc(sizeof(Node));
+		head = head->next;
+		
+	}
+	
+	head->data = data;
+    head->next = NULL;
+}
+
+int checkValid(int fd, char* input, int key)
+{
+	return EXIT_SUCCESS;
+}
+
+char* combine(Node* current, int count)
+{
+	char* total = malloc(BUFFSIZE*count + 2);
+	total[0] = '\0';
+    while(current!=NULL && current->data != NULL)
+    {
+		strcat(total, current->data);
+        current = current->next;
+    }
+	return total;
+}
+
+//returns the same thing as checkValid
+int readIn(int fd, int key)
+{	
+	int bytes;
+	char* buffHead = malloc(BUFFSIZE + 1);
+	buffHead[BUFFSIZE] = '\0';
+
+	if(fd<0) //invalid pathName passed
+	{
+		printf("error\n");
+	}
+	else
+	{	
+		for(int i = 0; i < 5 && (bytes =  read(fd, buffHead, BUFFSIZE)!=0 ; i++)
+		{
+			sleep(5);
+		}
+		if(bytes = 0) //nothing read in after 25 seconds
+		{
+			return EXIT_FAILURE;
+		}
+		
+		buffHead[bytes] = '\0';
+
+		if(bytes >= BUFFSIZE)
+		{
+			int count = 1;
+			Node head = {NULL, NULL};
+			
+			add(&head, buffHead);
+			
+			char* tempBuff = malloc(BUFFSIZE+1);
+			bytes =  read(fd, tempBuff, BUFFSIZE);
+			
+			tempBuff[bytes] = '\0';
+			while(bytes>0)
+			{
+				add(&head, tempBuff);
+				tempBuff = malloc(BUFFSIZE+1);
+				bytes =  read(fd, tempBuff, BUFFSIZE);
+				tempBuff[bytes] = '\0';
+				count++;
+			}
+			free(tempBuff);				
+			buffHead = combine(&head, count);
+			deleteList(&head);
+			
+		}	
+	}
+	//printf("%s\n", buffHead);
+	int exitStatus = checkValid(fd, buffHead, key);
+	free(buffHead);
+	
+	return exitStatus;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
