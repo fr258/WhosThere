a95bf5de5cbdb9111f3b60c2a3b208a548a73675
GabeGeytsman <59948329+GabeGeytsman@users.noreply.github.com>
2020-12-08 18:45:33 -0500

    Add files via upload
    Error handling working

--- Asst3.c
+++ Asst3.c
@@ -5,10 +5,17 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <ctype.h>
-
+#include <pthread.h>
 
 #define BACKLOG 5
 #define BUFFSIZE 256
+
+// the argument we will pass to the connection-handler threads
+struct connection {
+    struct sockaddr_storage addr;
+    socklen_t addr_len;
+    int fd;
+};
 
 typedef struct listNode
 {
@@ -25,6 +32,7 @@ typedef struct
 	
 } Iterator;
 
+void *echo(void *arg);
 int server(char *port);
 int joke(int sfd);
 int checkValid(int fd, char* input, int key);
@@ -51,7 +59,9 @@ int main(int argc, char **argv)
 int server(char *port)
 {
     struct addrinfo hint, *address_list, *addr;
+    struct connection *con;
     int error, sfd;
+    pthread_t tid;
 
     // initialize hints
     memset(&hint, 0, sizeof(struct addrinfo)); 
@@ -100,51 +110,43 @@ int server(char *port)
     // at this point sfd is bound and listening
     printf("Waiting for connection\n");
     for (;;) {
-
+    	// create argument struct for child thread
+		con = malloc(sizeof(struct connection));
+        con->addr_len = sizeof(struct sockaddr_storage);
+        	// addr_len is a read/write parameter to accept
+        	// we set the initial value, saying how much space is available
+        	// after the call to accept, this field will contain the actual address length
         
         // wait for an incoming connection
-        accept(sfd, NULL, NULL);
-
+        con->fd = accept(sfd, (struct sockaddr *) &con->addr, &con->addr_len);
+        	// we provide
+        	// sfd - the listening socket
+        	// &con->addr - a location to write the address of the remote host
+        	// &con->addr_len - a location to write the length of the address
+        	//
+        	// accept will block until a remote host tries to connect
+        	// it returns a new socket that can be used to communicate with the remote
+        	// host, and writes the address of the remote hist into the provided location
         
         // if we got back -1, it means something went wrong
-        if (sfd == -1) {
+        if (con->fd == -1) {
             perror("accept");
             continue;
-        }		
-		
-		//COMMENCE KNOCK KNOCK
-		
-		//knock knock
-        char* kkj1 = {"Knock, knock."};
-        write(sfd, kkj1, sizeof(kkj1)); 
-	
-		if(readIn(sfd,1)) //who's there?
-		{
-			close(sfd); //was invalid
-			continue;
-		}
-		
-		//setup
-        char* kkj2 = {"Incompetent interrupting cow."};
-        write(sfd, kkj2, sizeof(kkj2));
-		
-		if(readIn(sfd,3)) //setup, who?
-		{
-			close(sfd); //was invalid
-			continue;
-		}
-			
-		//punchline
-        char* kkj3 = {"...Moo!"};
-        write(sfd, kkj3, sizeof(kkj3));
-		
-		if(readIn(sfd,5)) //surprise/disgust
-		{
-			close(sfd); //was invalid
-			continue;
-		}
-			
-		close(sfd);
+        }
+
+		// spin off a worker thread to handle the remote connection
+        error = pthread_create(&tid, NULL, echo, con);
+
+		// if we couldn't spin off the thread, clean up and wait for another connection
+        if (error != 0) {
+            fprintf(stderr, "Unable to create thread: %d\n", error);
+            close(con->fd);
+            free(con);
+            continue;
+        }
+
+		// otherwise, detach the thread and wait for the next connection request
+        pthread_detach(tid);
     }
 
     return 0;
@@ -207,6 +209,7 @@ void add(Node* head, char* data)
 
 char* substring(char *out, const char *in, int startIndex, int length)
 {
+	//printf("entering substring\n");
     while (length > 0)
     {
         *out = *(in + startIndex);
@@ -220,18 +223,25 @@ char* substring(char *out, const char *in, int startIndex, int length)
 
 int checkFormat(char* input)
 {
+	printf("entering checkFormat\n");
     char* type = malloc(4);
     if(strlen(input)<3)
+    {
+        //printf("not long enough");
         return 1;
+    }
+        
     substring(type, input, 0, 3);
     if(strcmp(type, "REG")!=0 && strcmp(type, "ERR")!=0)
     {
+        //printf("does not start with reg/err");
         return 1;
     }
     else if(strcmp(type, "REG")==0)
     {
         if(input[3]!='|')
         {
+           // printf("no bar after reg");
             return 1;
         }
         else 
@@ -245,34 +255,41 @@ int checkFormat(char* input)
             }
             if(numCount==0)
             {
+                //printf("no numbers at all.");
                 return 1;
             }
             else
             {
                 if(input[index]!='|')
                 {
+                    //printf("missing bar after numbers.");
                     return 1;
                 }
                 else
                 {
-                    if(input[strlen(input)-1]!='|')
+                    int last = strlen(input);
+                    if(input[last-1]!='|' && !(input[last-3]=='|' && input[last-2]=='\r' && input[last-1]=='\n'))
                     {
+                        printf("missing bar at the end.\n");
+                        printf("The length is %ld and the last character is %c", strlen(input), input[strlen(input)-1]);
                         return 1;
                     }
                     else
                     {
+                        printf("valid");
                         return 0;
                     }
-                    
                 }
             }
         }
     }
+    printf("error message input");
     return -1;
 }
 
 int checkContent(char* input, char* correct)
 {
+	//printf("entering checkContent\n");
     int index = 0;
     while(!isdigit(input[index]))
     {
@@ -299,6 +316,7 @@ int checkContent(char* input, char* correct)
 
 int checkLen(char* input)
 {
+	//printf("entering checklen\n");
     int index = 0;
     while(!isdigit(input[index]))
     {
@@ -326,9 +344,10 @@ int checkLen(char* input)
 
 int checkValid(int sfd, char* input, int key)
 {
+	//printf("entering checkValid\n");
     if(key==1)
     {
-        if(!checkFormat(input))
+        if(checkFormat(input))
         {
             if(checkFormat(input)==-1)
             {
@@ -355,7 +374,7 @@ int checkValid(int sfd, char* input, int key)
     }
     else if(key==3)
     {
-        if(!checkFormat(input))
+        if(checkFormat(input)!=0)
         {
             if(checkFormat(input)==-1)
             {
@@ -381,7 +400,7 @@ int checkValid(int sfd, char* input, int key)
     }
     else if(key==5)
     {
-        if(!checkFormat(input))
+        if(checkFormat(input)!=0)
         {
             if(checkFormat(input)==-1)
             {
@@ -398,7 +417,7 @@ int checkValid(int sfd, char* input, int key)
             write(sfd, err5, sizeof(err5));
             return 0;
         }
-        if(!ispunct(input[strlen(input)-2]))
+        if(ispunct(input[strlen(input)-2]))
         {
             char* err5 = {"ERR|M1CT|"};
             write(sfd, err5, sizeof(err5));
@@ -434,13 +453,14 @@ int readIn(int fd, int key)
 	}
 	else
 	{	
+		//sleep(5);
 		bytes =  read(fd, buffHead, BUFFSIZE);
-
+		//bytes =  read(fd, buffHead, BUFFSIZE);
+		buffHead[bytes] = '\0';
 		if(bytes == 0) //nothing read in
 		{
 			return EXIT_FAILURE;
 		}
-		
 		buffHead[bytes] = '\0';
 
 		if(bytes >= BUFFSIZE)
@@ -451,27 +471,99 @@ int readIn(int fd, int key)
 			add(&head, buffHead);
 			
 			char* tempBuff = malloc(BUFFSIZE+1);
-			bytes =  read(fd, tempBuff, BUFFSIZE);
 			
+			bytes =  read(fd, tempBuff, BUFFSIZE);	
 			tempBuff[bytes] = '\0';
+			
 			while(bytes>0)
 			{
 				add(&head, tempBuff);
 				tempBuff = malloc(BUFFSIZE+1);
+				
 				bytes =  read(fd, tempBuff, BUFFSIZE);
 				tempBuff[bytes] = '\0';
+				
 				count++;
 			}
-			free(tempBuff);				
+			//free(tempBuff);				
 			buffHead = combine(&head, count);
-			deleteList(&head);
+			//deleteList(&head);
 			
 		}	
 	}
-	//printf("%s\n", buffHead);
 	int exitStatus = checkValid(fd, buffHead, key);
-	free(buffHead);
-	
+	//free(buffHead);
 	return exitStatus;
+	return 0;
 
+}
+void *echo(void *arg)
+{
+    struct connection *c = (struct connection *) arg;
+
+	// find out the name and port of the remote host
+   /* error = getnameinfo((struct sockaddr *) &c->addr, c->addr_len, host, 100, port, 10, NI_NUMERICSERV);
+    	// we provide:
+    	// the address and its length
+    	// a buffer to write the host name, and its length
+    	// a buffer to write the port (as a string), and its length
+    	// flags, in this case saying that we want the port as a number, not a service name
+    if (error != 0) {
+        fprintf(stderr, "getnameinfo: %s", gai_strerror(error));
+        close(c->fd);
+        return NULL;
+    }
+
+    printf("[%s:%s] connection\n", host, port);*/
+
+    /*while ((nread = read(c->fd, buf, 100)) > 0) {
+        buf[nread] = '\0';
+        printf("[%s:%s] read %d bytes |%s|\n", host, port, nread, buf);
+    }
+
+    printf("[%s:%s] got EOF\n", host, port);
+
+    close(c->fd);
+    free(c);
+    return NULL;*/
+	joke(c->fd);
+	close(c->fd);
+    free(c);
+	
+	return NULL;
+	
+	
+}
+int joke(int sfd)
+{
+	    char* kkj1 = {"REG|13|Knock, knock.|"};
+        write(sfd, kkj1, strlen(kkj1)); 
+	
+		if(!readIn(sfd,1)) //who's there?
+		{
+			//close(sfd); //was invalid
+			return 0;
+		}
+		
+		 //setup
+        char* kkj2 = {"REG|29|Incompetent interrupting cow.|"};
+        write(sfd, kkj2, strlen(kkj2));
+		
+		if(!readIn(sfd,3)) //setup, who?
+		{
+			//close(sfd); //was invalid
+			return 0;
+		}
+			
+		//punchline
+        char* kkj3 = {"REG|7|...Moo!|"};
+        write(sfd, kkj3, strlen(kkj3));
+		
+		if(!readIn(sfd,5)) //surprise/disgust
+		{
+			//close(sfd); //was invalid
+			return 0;
+		}		
+		//close(sfd);
+		return 0;
 }
